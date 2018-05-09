64fc83e79bdbe6d659cc00a24a5507eeb9e73972
kde0820 <kde0820@naver.com>
2018-05-09 21:04:35 +0900

    Solve '수박수박수박수박수박수?' with Python

---
+++ Level_1/수박수박수박수박수박수?/main.py
@@ -0,0 +1,14 @@
+def water_melon(n):
+    # 함수를 완성하세요.
+    result = ""
+    for i in range(n):
+        if i % 2 == 0:
+            result += '수'
+        else:
+            result += '박'
+    return result
+
+
+# 실행을 위한 테스트코드입니다.
+print("n이 3인 경우: " + water_melon(3));
+print("n이 4인 경우: " + water_melon(4));
