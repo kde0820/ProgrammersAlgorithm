666d07cc0652ddab958d54ff9520ac8432f560b9
kde0820 <kde0820@naver.com>
2018-05-09 21:05:42 +0900

    Solve '수박수박수박수박수박수?' with Python

--- Level_1/수박수박수박수박수박수?/main.py
+++ Level_1/수박수박수박수박수박수?/main.py
@@ -10,5 +10,5 @@ def water_melon(n):
 
 
 # 실행을 위한 테스트코드입니다.
-print("n이 3인 경우: " + water_melon(3));
-print("n이 4인 경우: " + water_melon(4));
+print("n이 3인 경우: " + water_melon(3))
+print("n이 4인 경우: " + water_melon(4))
