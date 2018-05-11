e63e760c872b8ee65d2908affa1f2c200a7d9ab9
kde0820 <kde0820@naver.com>
2018-05-11 15:52:24 +0900

    Solve '정수제곱근판별하기' with Python

---
+++ Level_1/정수제곱근판별하기/main.py
@@ -0,0 +1,9 @@
+import math
+def nextSqure(n):
+    # 함수를 완성하세요
+    if math.sqrt(n) == int(math.sqrt(n)):
+        return int(pow(math.sqrt(n)+1, 2))
+    return 'no'
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print("결과 : {}".format(nextSqure(121)))
