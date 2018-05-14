37332ab96f00dc87304ebe586bce71f6e506e0a4
kde0820 <kde0820@naver.com>
2018-05-14 15:08:39 +0900

    Solve '멀리 뛰기' with Python

---
+++ Level_3/멀리 뛰기/main.py
@@ -0,0 +1,10 @@
+def jumpCase(num):
+    if num < 3:
+        return num
+    a, b = 1, 2
+    for i in range(3, num+1):
+        a, b = b, a + b
+    return b
+
+#아래는 테스트로 출력해 보기 위한 코드입니다.
+print(jumpCase(4))
