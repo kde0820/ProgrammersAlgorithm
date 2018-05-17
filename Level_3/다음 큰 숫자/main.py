84f53e703cb4c3716e08e3a3ec4cbdbe022c15ab
kde0820 <kde0820@naver.com>
2018-05-17 23:13:45 +0900

    Solve '다음 큰 숫자' with python

---
+++ Level_3/다음 큰 숫자/main.py
@@ -0,0 +1,8 @@
+def nextBigNumber(n):
+    one_count = bin(n).count('1')
+    n += 1
+    while bin(n).count('1') != one_count:
+        n += 1
+    return n
+#아래 코드는 테스트를 위한 출력 코드입니다.
+print(nextBigNumber(78))
