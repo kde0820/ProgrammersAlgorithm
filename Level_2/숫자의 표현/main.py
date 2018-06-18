69d7c787f1a9e115d10f56f0ff97f7f9cb2e06dd
kde0820 <kde0820@naver.com>
2018-06-18 14:52:06 +0900

    Update '숫자의 표현'

--- Level_2/숫자의 표현/main.py
+++ Level_2/숫자의 표현/main.py
@@ -1,18 +1,12 @@
-# -*- coding: utf-8 -*-
-def expressions(num):
+def solution(n):
     answer = 1
-    for i in range(num - 1, 1, -1):
-        print("i: ", i)
+    for i in range(n - 1, 1, -1):
         j = i
-        n = num
-        while j - 1 > 0 and (n - j) >= (j - 1):
-            n -= j
+        num = n
+        while j - 1 > 0 and (num - j) >= (j - 1):
+            num -= j
             j -= 1
-            if n - j == 0:
+            if num - j == 0:
                 answer += 1
                 break 
     return answer
-
-
-# 아래는 테스트로 출력해 보기 위한 코드입니다.
-print(expressions(15))
