89d02d3457b8a3d97e663f5a4d53aeef89fabe09
kde0820 <kde0820@naver.com>
2018-06-18 14:52:38 +0900

    Update '야근지수' - 시간초과

--- Level_2/야근 지수/main.py
+++ Level_2/야근 지수/main.py
@@ -1,10 +1,6 @@
-def noOvertime(n, works):
-    result = 0
-    # 야근 지수를 최소화 하였을 때의 야근 지수는 몇일까요?
+def solution(n, works):
+    if sum(works) <= n:
+        return 0
     for i in range(n):
         works[works.index(max(works))] -= 1
-    for num in works:
-        result += num*num
-    return result
-
-print(noOvertime(4, [4, 3, 3]))
+    return sum([i*i for i in works])
